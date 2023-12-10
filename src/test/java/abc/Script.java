c30b7069cc934ae1683b891e144c79134f7ce159
madha <madhavmane>
2023-12-10 17:24:50 +0530

    headless code

--- src/test/java/abc/Script.java
+++ src/test/java/abc/Script.java
@@ -1,20 +1,21 @@
 package abc;
 
 import java.time.Duration;
-
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class Script {
 
 	public static void main(String[] args) {
 		
-
-	WebDriver driver=new ChromeDriver();	
+	ChromeOptions options = new ChromeOptions();
+    options.addArguments("--headless");
+	WebDriver driver=new ChromeDriver(options);	
 	driver.get("https://www.youtube.com/");	
 	driver.manage().window().maximize();
 	driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
