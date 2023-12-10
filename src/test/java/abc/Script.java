a27c1f0c8dd317f219ebca023fc8336250413af9
madhavamane <madhavmane111@gmail.com>
2023-12-10 12:35:44 +0530

    This is first

---
+++ src/test/java/abc/Script.java
@@ -0,0 +1,28 @@
+package abc;
+
+public class Script {
+
+	public static void main(String[] args) {
+		
+
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+}
---
+++ src/test/java/abc/Test.java
@@ -0,0 +1,29 @@
+package abc;
+
+public class Test {
+
+	public static void main(String[] args) {
+		
+   System.out.println("$");	
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+
+	}
+}
